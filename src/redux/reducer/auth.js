4b6a54bda09883b14585714b0e6d7c3844432d31
Alexey Struchenkov <forcorp@inbox.ru>
2021-03-09 15:56:08 +0200

    remove token to localStorage

--- src/redux/reducer/auth.js
+++ src/redux/reducer/auth.js
@@ -1,66 +1,68 @@
 import {
-  ACTION, EDIT,
+  ACTION,
+  EDIT,
   FAILURE,
   LOGIN,
   LOGOUT,
   PASSWORD,
   REQUEST,
   SET,
-  SUCCESS, TASK,
-  USERNAME
+  SUCCESS,
+  TASK,
+  USERNAME,
 } from "../constants";
 
 const authInitialState = {
-  username: '',
-  password: '',
-  token: null,
+  username: "",
+  password: "",
   loading: false,
   error: null,
-}
+};
 const auth = (state = authInitialState, action) => {
-  const {type} = action;
+  const { type } = action;
 
   switch (type) {
-
     case LOGIN + ACTION + REQUEST:
-      return {...state, error: null, loading: true }
+      return { ...state, error: null, loading: true };
 
     case LOGIN + ACTION + SUCCESS:
-      const {token} = action
-      return {...state, loading: false, token,  password: ''  }
+      const { token } = action;
+      localStorage.setItem("token", token);
+      return { ...state, loading: false, password: "" };
 
     case LOGIN + ACTION + FAILURE:
-      const {error} = action
-      return {...state, loading: false, password: '', error }
+      const { error } = action;
+      return { ...state, loading: false, password: "", error };
 
     case LOGOUT + ACTION:
-      return {...state, login: "", token: null }
+      localStorage.removeItem("token");
+      return { ...state, login: "" };
 
     case USERNAME + SET:
       const userError = state.error?.username;
       if (userError) {
-        const newError = {...state.error};
-        delete newError.username
-        return {...state, username: action.username, error: {...newError}}
+        const newError = { ...state.error };
+        delete newError.username;
+        return { ...state, username: action.username, error: { ...newError } };
       }
-      return  {...state, username: action.username}
+      return { ...state, username: action.username };
 
     case PASSWORD + SET:
       const passwordError = state.error?.password;
       if (passwordError) {
-        const newError = {...state.error};
-        delete newError.password
-        return {...state, password: action.password, error: {...newError}}
+        const newError = { ...state.error };
+        delete newError.password;
+        return { ...state, password: action.password, error: { ...newError } };
       }
-      return  {...state, password: action.password}
+      return { ...state, password: action.password };
 
     case TASK + EDIT + FAILURE:
       if (action?.error?.token) {
-        return {...state, token: null}
+        return { ...state, token: null };
       }
-      return state
+      return state;
     default:
       return state;
   }
-}
+};
 export default auth;
--- src/redux/selectors/auth.js
+++ src/redux/selectors/auth.js
@@ -1,6 +1,4 @@
-
-
-export const tokenSelector = (state) => state.auth.token;
+export const tokenSelector = (state) => localStorage.getItem("token") || null;
 export const authUserNameSelector = (state) => state.auth.username;
 export const authPasswordSelector = (state) => state.auth.password;
 export const authLoadingSelector = (state) => state.auth.loading;
